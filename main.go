4e2648fefd8b42b04a0f729b1673c40ec54b96ef
Eugen Sorin PASCALE <eugen@ctce.ro>
2016-06-07 20:57:22 +0300

    Main file

--- main.go
+++ main.go
@@ -2,34 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-    s := []int{2, 3, 5, 7, 11, 13}
-    printSlice("s", s)
-
-	a := make([]int, 5)
-	printSlice("a", a)
-
-	b := make([]int, 0, 5)
-	printSlice("b", b)
-
-	c := b[0:2]
-	printSlice("c", c)
-
-	d := c[2:4]
-	printSlice("d", d)
-
-    e := a[0:2]
-    printSlice("e", e)
-
-    f := e[2:4]
-    printSlice("f", f)
-
-    d[0] = 1; f[0] = 2;
-    fmt.Println(a, b, c, d, e, f)
-
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	var x1, x2 int = 1, 1
+	return func () int {
+		x := x1
+		x1 = x2
+		x2 += x
+		return x
+	}
 }
 
-func printSlice(s string, x []int) {
-	fmt.Printf("%s %T len=%d cap=%d %v\n",
-		s, x, len(x), cap(x), x)
+func main() {
+	f, g := fibonacci(), fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f(), g(), g())
+	}
 }
