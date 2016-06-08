b94edbd4d7d2b4b7d4441156fae074ab4491ad09
Eugen Sorin PASCALE <eugen@ctce.ro>
2016-06-08 15:42:27 +0300

    Main file

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"os"
+    "fmt"
+	"strings"
+)
 
 // fibonacci is a function that returns
 // a function that returns an int.
@@ -14,9 +19,41 @@ func fibonacci() func() int {
 	}
 }
 
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(buf []byte) (int, error) {
+	k, err := rot.r.Read(buf)
+	for i := 0 ; i < k ; i++ {
+		c := buf[i]
+		if 'a' <= c && c <= 'z' {
+			c += 13
+			if c > 'z' {
+				c = 'a' + c - 'z' - 1
+			}
+			buf[i] = c
+		}
+		if 'A' <= c && c <= 'Z' {
+			c += 13
+			if c > 'Z' {
+				c = 'A' + c - 'Z' - 1
+			}
+			buf[i] = c
+		}
+	}
+	return k, err
+}
+
+
 func main() {
 	f, g := fibonacci(), fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f(), g(), g())
+		fmt.Println(f(), g())
 	}
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+
 }
